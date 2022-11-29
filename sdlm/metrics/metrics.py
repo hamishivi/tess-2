12c049582c49ab0736307403dbb99cbef6b06340
rabeeh karimi mahabadi <rabeehk@allenai.org>
2022-11-28 19:19:41 -0800

    Adds the zipf coefficient

--- sdlm/metrics/metrics.py
+++ sdlm/metrics/metrics.py
@@ -2,6 +2,9 @@
 from mauve import compute_mauve
 from nltk.util import ngrams
 import numpy as np
+from collections import Counter
+from scipy import stats
+import operator
 
 MAX_TEXT_LENGTH = 256
 
@@ -37,3 +40,20 @@ def distinct_n_grams(texts):
         dist_2.append(len(bigrams) / total_words)
         dist_3.append(len(trigrams) / total_words)
     return np.nanmean(dist_1), np.nanmean(dist_2), np.nanmean(dist_3)
+
+
+def zipf(tokenized_texts, N=5000):
+    """Computes the Zipf coefficient.
+
+    Args:
+        tokenized_texts (List[List[int]]) tokenized texts.
+    Adapted from https://github.com/ari-holtzman/degen/blob/master/metrics/zipf.py
+    """
+    cnt = Counter()
+    for tokenized_text in tokenized_texts:
+        cnt.update(tokenized_text)
+
+    xs = np.arange(1, min(len(cnt), N) + 1)
+    ys = np.array(sorted(cnt.values(), key=operator.neg)[:N])
+    a, b, r, p, std = stats.linregress(np.log(xs), np.log(ys))
+    return -a, -r, p
