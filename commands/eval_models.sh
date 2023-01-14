008ed891099822c174846320385d30dadae65cd0
rabeeh karimi mahabadi <rabeehk@allenai.org>
2023-01-14 07:50:00 +0000

    updated

--- commands/eval_models.sh
+++ commands/eval_models.sh
@@ -102,6 +102,7 @@ do
 done
 '
 
+: '
 # Run one example without using skip_special_tokens.
 truncation_length=206
 for TOP_P in 0.95 0.99
@@ -116,16 +117,13 @@ do
       CUDA_VISIBLE_DEVICES=0 python compute_mlm_metrics.py --model_name_or_path ${model_path} --truncation_length ${truncation_length} --output_dir ${output_dir} ${shared_params} ${PARAMS_FOR_LOCAL} ${extra_params}  --eval_for_all_metrics --max_seq_length 256 --truncation_length 206 --max_eval_samples 1000 --per_device_eval_batch_size 25 --temperature ${TEMPERATURE} --top_p ${TOP_P} --conditional_generation "prefix_lm" --skip_special_tokens False
    done
 done
-
-
-
+'
 ##########################################################
 # Evaluate the type of self-conditioning.
-: '
 TEMPERATURE=1
 for TOP_P in 0.95 0.99  0.9
 do
-      checkpoint="/checkpoint-13000/"
+      checkpoint="/checkpoint-24000/" #13000/"
       model_path="self_condition_max/"${checkpoint}
       output_dir=$BASE_DIR"/outputs/paper_experiments/self_condition/tune_length_25_context_25_truncation_"${truncation_length}"/ul2_self_condition_with_max_top_p_"${TOP_P}"_temperature_"${TEMPERATURE}""${checkpoint}
       python -m torch.distributed.launch --nproc_per_node 4 run_mlm.py --model_name_or_path ${model_path} --truncation_length ${truncation_length} --output_dir ${output_dir} ${shared_params} ${PARAMS_FOR_LOCAL} ${extra_params} --self_condition logits_max --max_seq_length 256 --truncation_length 206 --max_eval_samples 1000 --per_device_eval_batch_size 25 --temperature ${TEMPERATURE} --top_p ${TOP_P}
@@ -139,7 +137,7 @@ done
 TEMPERATURE=1
 for TOP_P in 0.95 0.99  0.9
 do
-      checkpoint="/checkpoint-13000/"
+      checkpoint="/checkpoint-24000/" #13000/"
       model_path="self_condition_mean/"${checkpoint}
       output_dir=$BASE_DIR"/outputs/paper_experiments/self_condition/tune_length_25_context_25_truncation_"${truncation_length}"/ul2_self_condition_mean_top_p_"${TOP_P}"_temperature_"${TEMPERATURE}""${checkpoint}
       python -m torch.distributed.launch --nproc_per_node 4 run_mlm.py --model_name_or_path ${model_path} --truncation_length ${truncation_length} --output_dir ${output_dir} ${shared_params} ${PARAMS_FOR_LOCAL} ${extra_params} --self_condition logits_mean --max_seq_length 256 --truncation_length 206 --max_eval_samples 1000 --per_device_eval_batch_size 25 --temperature ${TEMPERATURE} --top_p ${TOP_P}
@@ -154,7 +152,7 @@ done
 TEMPERATURE=1
 for TOP_P in 0.95 0.99  0.9
 do
-      checkpoint="/checkpoint-15000/"
+      checkpoint="/checkpoint-46000/" #15000/"
       model_path="self_condition_with_addition/"${checkpoint}
       output_dir=$BASE_DIR"/outputs/paper_experiments/self_condition/tune_length_25_context_25_truncation_"${truncation_length}"/ul2_self_condition_with_addition_top_p_"${TOP_P}"_temperature_"${TEMPERATURE}""${checkpoint}
       python -m torch.distributed.launch --nproc_per_node 4 run_mlm.py --model_name_or_path ${model_path} --truncation_length ${truncation_length} --output_dir ${output_dir} ${shared_params} ${PARAMS_FOR_LOCAL} ${extra_params} --self_condition logits_addition --max_seq_length 256 --truncation_length 206 --max_eval_samples 1000 --per_device_eval_batch_size 25 --temperature ${TEMPERATURE} --top_p ${TOP_P}
@@ -164,7 +162,20 @@ do
       output_dir=$BASE_DIR"/outputs/paper_experiments/self_condition/tune_length_25_context_25_truncation_"${truncation_length}"/ul2_self_condition_with_addition_top_p_"${TOP_P}"_temperature_"${TEMPERATURE}""${checkpoint}
       CUDA_VISIBLE_DEVICES=0 python compute_mlm_metrics.py --model_name_or_path ${model_path} --truncation_length ${truncation_length} --output_dir ${output_dir} ${shared_params} ${PARAMS_FOR_LOCAL} ${extra_params} --self_condition logits_addition  --eval_for_all_metrics --max_seq_length 256 --truncation_length 206 --max_eval_samples 1000 --per_device_eval_batch_size 25 --temperature ${TEMPERATURE} --top_p ${TOP_P}
 done
-'
+
+TEMPERATURE=1
+for TOP_P in 0.95 0.99  0.9
+do
+      checkpoint="/checkpoint-46000/" #15000/"
+      model_path="self_condition/"${checkpoint}
+      output_dir=$BASE_DIR"/outputs/paper_experiments/self_condition/tune_length_25_context_25_truncation_"${truncation_length}"/ul2_self_condition_top_p_"${TOP_P}"_temperature_"${TEMPERATURE}""${checkpoint}
+      python -m torch.distributed.launch --nproc_per_node 4 run_mlm.py --model_name_or_path ${model_path} --truncation_length ${truncation_length} --output_dir ${output_dir} ${shared_params} ${PARAMS_FOR_LOCAL} ${extra_params} --self_condition logits --max_seq_length 256 --truncation_length 206 --max_eval_samples 1000 --per_device_eval_batch_size 25 --temperature ${TEMPERATURE} --top_p ${TOP_P}
+done 
+for TOP_P in 0.95 0.99 0.9
+do
+      output_dir=$BASE_DIR"/outputs/paper_experiments/self_condition/tune_length_25_context_25_truncation_"${truncation_length}"/ul2_self_condition_top_p_"${TOP_P}"_temperature_"${TEMPERATURE}""${checkpoint}
+      CUDA_VISIBLE_DEVICES=0 python compute_mlm_metrics.py --model_name_or_path ${model_path} --truncation_length ${truncation_length} --output_dir ${output_dir} ${shared_params} ${PARAMS_FOR_LOCAL} ${extra_params} --self_condition logits --eval_for_all_metrics --max_seq_length 256 --truncation_length 206 --max_eval_samples 1000 --per_device_eval_batch_size 25 --temperature ${TEMPERATURE} --top_p ${TOP_P}
+done
 
 
 #######################################
