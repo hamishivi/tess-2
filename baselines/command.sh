22e5997f007e75a40062d9ce7d29e7fb772bfdc7
rabeeh karimi mahabadi <rabeehk@allenai.org>
2023-01-15 20:36:34 -0800

    updated

--- baselines/command.sh
+++ baselines/command.sh
@@ -8,4 +8,6 @@ python run_simplification.py \
     --per_device_train_batch_size=4 \
     --per_device_eval_batch_size=4 \
     --overwrite_output_dir \
-    --predict_with_generate
+    --predict_with_generate \
+    --eval_steps 10 \
+    --evaluation_strategy steps
