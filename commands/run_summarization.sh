8606670f3ec5f090617da79e8adcc323c61a1489
rabeeh karimi mahabadi <rabeehk@allenai.org>
2023-01-20 01:10:31 -0800

    updated

--- commands/run_summarization.sh
+++ commands/run_summarization.sh
@@ -7,7 +7,10 @@ python run_summarization.py --model_name_or_path roberta-large --do_train --do_e
 '
 
 # data length=512
-python -m torch.distributed.launch --nproc_per_node 4 run_summarization.py --model_name_or_path roberta-large --do_train --do_eval --dataset_name xsum --dataset_config "3.0.0" --output_dir /net/nfs.cirrascale/s2-research/rabeehk/outputs/paper_experiments/summarization_512_self_condition_mean --per_device_train_batch_size=6 --per_device_eval_batch_size=15 --overwrite_output_dir  --report_to tensorboard --eval_steps 1000  --max_steps 1000000 --max_eval_samples 96 --max_source_length 512  --max_target_length 120 --max_seq_length 632 --conditional_generation "seq2seq" --num_inference_diffusion_steps 1000 --evaluation_strategy steps --simplex_value 5 --num_diffusion_steps 5000 --lr_scheduler_type linear --learning_rate 1e-4 --pad_to_max_length  --beta_schedule squaredcos_improved_ddpm --weight_decay 0.01 --top_p 0.99 --warmup_steps 2000 --logging_steps 50 --save_steps 1000 ${PARAMS_FOR_LOCAL} --resize_position_embeddings True --fp16 --self_condition "logits_mean" --gradient_accumulation_steps 2
+learning_rate=5e-5
+python -m torch.distributed.launch --nproc_per_node 4 run_summarization.py --model_name_or_path roberta-large --do_train --do_eval --dataset_name xsum --dataset_config "3.0.0" --output_dir "/net/nfs.cirrascale/s2-research/rabeehk/outputs/paper_experiments/summarization_results/ours_lr_"${learning_rate} --per_device_train_batch_size=6 --per_device_eval_batch_size=12 --overwrite_output_dir  --report_to tensorboard --eval_steps 1000  --max_steps 100000 --max_eval_samples 96 --max_source_length 512  --max_target_length 120 --max_seq_length 632 --conditional_generation "seq2seq" --num_inference_diffusion_steps 1000 --evaluation_strategy steps --simplex_value 5 --num_diffusion_steps 5000 --lr_scheduler_type linear --learning_rate ${learning_rate} --pad_to_max_length  --beta_schedule squaredcos_improved_ddpm --weight_decay 0.0 --top_p 0.99 --warmup_steps 2000 --logging_steps 50 --save_steps 1000 ${PARAMS_FOR_LOCAL} --resize_position_embeddings True  --self_condition "logits_mean" --self_condition_mix_before_weights true  --gradient_accumulation_steps 2  --save_checkpoints_on_s3
+
+
 
 : '
 # data length=1024
