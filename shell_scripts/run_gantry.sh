4af8444fd388ad14a27fa061096b52b101f07c40
Hamish Ivison <hamishivi@gmail.com>
2023-06-28 19:50:18 +0000

    script updates

--- shell_scripts/run_gantry.sh
+++ shell_scripts/run_gantry.sh
@@ -44,7 +44,7 @@ EXP_NAME="prefix_uncond_100k_c4_roberta_base_shorter_gen"
 #         --dataset_name c4 --streaming --dataset_config_name en
 
 
-EXP_NAME="ul2_orig_100k_c4_roberta_base_long_context_short_gen"
+EXP_NAME="ul2_orig_100k_c4_roberta_base_preatrain_50k"
 
 gantry run -y -n $EXP_NAME -t $EXP_NAME --allow-dirty \
     --workspace ai2/tess2 \
@@ -57,7 +57,7 @@ gantry run -y -n $EXP_NAME -t $EXP_NAME --allow-dirty \
     --beaker-image 'ai2/pytorch2.0.0-cuda11.8-python3.10' \
     --venv 'base' \
     --pip requirements.txt \
-    -- python sdlm/run_mlm.py \
+    -- python -m sdlm.run_mlm \
         --model_name_or_path roberta-base \
         --per_device_train_batch_size 48  \
         --per_device_eval_batch_size 48 \
@@ -68,7 +68,6 @@ gantry run -y -n $EXP_NAME -t $EXP_NAME --allow-dirty \
         --eval_steps 1000 \
         --report_to tensorboard \
         --max_seq_length 256  \
-        --eval_context_size 231 \
         --max_eval_samples 512 \
         --simplex_value 5 \
         --num_diffusion_steps 5000  \
@@ -79,9 +78,9 @@ gantry run -y -n $EXP_NAME -t $EXP_NAME --allow-dirty \
         --beta_schedule squaredcos_improved_ddpm \
         --weight_decay 0.01 \
         --top_p 0.99 \
-        --max_steps 100000 \
+        --max_steps 50000 \
         --gradient_accumulation_steps 4 \
-        --warmup_steps 2000 \
+        --warmup_ratio 0.05 \
         --logging_steps 50 \
         --save_steps 1000 \
         --conditional_generation ul2 \
--- shell_scripts/test.sh
+++ shell_scripts/test.sh
@@ -6,7 +6,7 @@ python -m sdlm.run_mlm \
         --do_eval \
         --output_dir tmp \
         --evaluation_strategy steps \
-        --eval_steps 1000 \
+        --eval_steps 10000 \
         --report_to tensorboard \
         --max_seq_length 512  \
         --max_eval_samples 512 \
@@ -23,10 +23,9 @@ python -m sdlm.run_mlm \
         --gradient_accumulation_steps 4 \
         --warmup_steps 2000 \
         --logging_steps 50 \
-        --save_steps 1000 \
+        --save_steps 11000 \
         --conditional_generation ul2 \
         --self_condition "logits_mean" \
         --self_condition_mix_before_weights \
         --dataset_name c4 --streaming --dataset_config_name en \
-        --overwrite_output_dir \
-        --token_warp
+        --overwrite_output_dir
