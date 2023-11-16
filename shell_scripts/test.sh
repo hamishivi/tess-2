b0a68cf922da29e50c09d752887157ee88fa0672
Jake Tae <jaket@allennlp-cirrascale-50.reviz.ai2.in>
2023-11-16 05:32:47 +0000

    feature: add noise multiplier

--- shell_scripts/test.sh
+++ shell_scripts/test.sh
@@ -4,29 +4,28 @@ python -m sdlm.run_mlm \
         --per_device_eval_batch_size 24 \
         --do_train \
         --do_eval \
-        --output_dir tmp \
+        --output_dir outputs/noise/1500_5000 \
         --evaluation_strategy steps \
         --eval_steps 1000 \
-        --num_inference_diffusion_steps 100 \
         --report_to tensorboard \
         --max_seq_length 512  \
         --max_eval_samples 24 \
         --simplex_value 5 \
         --num_diffusion_steps 5000  \
+        --num_inference_diffusion_steps 100 \
         --lr_scheduler_type cosine \
-        --learning_rate 1e-4 \
+        --learning_rate 1e-5 \
         --pad_to_max_length \
         --beta_schedule squaredcos_improved_ddpm \
         --weight_decay 0.01 \
         --top_p 0.99 \
-        --max_steps 100000 \
-        --gradient_accumulation_steps 1 \
-        --warmup_steps 200 \
+        --max_steps 50000 \
+        --gradient_accumulation_steps 16 \
+        --warmup_ratio 0.05 \
         --logging_steps 50 \
         --save_steps 1000 \
         --conditional_generation ul2 \
         --self_condition "logits_mean" \
         --self_condition_mix_before_weights \
         --dataset_name c4 --streaming --dataset_config_name en \
-        --use_model "cdcd" \
         --overwrite_output_dir
