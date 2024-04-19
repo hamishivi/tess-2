809f65d84ab897639da4078c6e11bc0edcfacfd2
Jake Tae <jaket@allennlp-cirrascale-50.reviz.ai2.in>
2024-04-19 03:58:33 +0000

    chore: add llama sni beaker cmd

--- shell_scripts/run_sni.sh
+++ shell_scripts/run_sni.sh
@@ -1,4 +1,4 @@
-# experimenting over sni
+# roberta diffusion sni baseline
 gantry run -y -n sni_tess_self_conf -t sni_tess_self_conf --allow-dirty \
     --workspace ai2/tess2 \
     --nfs \
@@ -8,7 +8,6 @@ gantry run -y -n sni_tess_self_conf -t sni_tess_self_conf --allow-dirty \
     --cluster ai2/allennlp-cirrascale \
     --env 'HF_HOME=/net/nfs.cirrascale/allennlp/hamishi/.hf' \
     --env 'PROTOCOL_BUFFERS_PYTHON_IMPLEMENTATION=python' \
-    --beaker-image 'ai2/pytorch2.0.0-cuda11.8-python3.10' \
     --venv 'base' \
     --pip requirements.txt \
     -- python -m sdlm.run_glue \
@@ -44,3 +43,55 @@ gantry run -y -n sni_tess_self_conf -t sni_tess_self_conf --allow-dirty \
         --preprocessing_num_workers 16 \
         --self_condition "logits_mean" \
         --self_condition_mix_before_weights
+
+# llama diffusion sni
+gantry run -y -n sni_llama_test -t sni_llama_test --allow-dirty \
+    --workspace ai2/tess2 \
+    --nfs \
+    --gpus 1 \
+    --priority normal \
+    --budget ai2/allennlp \
+    --cluster ai2/allennlp-cirrascale \
+    --env 'HF_HOME=/net/nfs.cirrascale/allennlp/jaket/.hf' \
+    --env 'PROTOCOL_BUFFERS_PYTHON_IMPLEMENTATION=python' \
+    --venv 'base' \
+    --pip requirements.txt \
+    -- python -m sdlm.run_glue \
+        --model_name_or_path meta-llama/Llama-2-7b-hf \
+        --dataset_name sni \
+        --output_dir /results \
+        --do_train \
+        --do_eval \
+        --max_seq_length 512 \
+        --per_device_train_batch_size 16 \
+        --skip_special_tokens true \
+        --per_device_eval_batch_size 16 \
+        --evaluation_strategy steps \
+        --eval_steps 100 \
+        --save_strategy steps \
+        --report_to tensorboard \
+        --overwrite_output_dir \
+        --pad_to_max_length \
+        --simplex_value 5 \
+        --num_train_epochs 2 \
+        --num_diffusion_steps 5000 \
+        --num_inference_diffusion_steps 100 \
+        --conditional_generation seq2seq \
+        --learning_rate 3e-5 \
+        --gradient_accumulation_steps 4 \
+        --lr_scheduler_type cosine \
+        --beta_schedule squaredcos_improved_ddpm \
+        --top_p 0.99 \
+        --warmup_ratio 0.03 \
+        --logging_steps 50 \
+        --save_total_limit 1 \
+        --max_eval_samples 512 \
+        --preprocessing_num_workers 16 \
+        --self_condition "logits_mean" \
+        --self_condition_mix_before_weights \
+        --bf16 \
+        --optim adamw_torch_fused \
+        --gradient_checkpointing \
+        --use_flash_attention2 \
+        --is_causal false \
+        --mask_padding_in_loss false
