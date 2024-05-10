bfd571434db0f78614e35499adcc3be60910f5ad
Jake Tae <jaket@allennlp-cirrascale-50.reviz.ai2.in>
2024-05-10 21:43:29 +0000

    chore: cleanup ar trainer

--- sdlm/trainer_ar.py
+++ sdlm/trainer_ar.py
@@ -3,10 +3,10 @@ from typing import Dict, List, NamedTuple, Optional, Tuple, Union
 import numpy as np
 import torch
 from torch.utils.data import DataLoader, Dataset
-from transformers import Seq2SeqTrainer, Trainer
+from transformers import Seq2SeqTrainer
 from transformers.integrations import TensorBoardCallback
 from transformers.trainer_utils import seed_worker
-from transformers.utils import is_datasets_available, is_sagemaker_mp_enabled, logging
+from transformers.utils import is_datasets_available, logging
 
 if is_datasets_available():
     import datasets
@@ -123,67 +123,3 @@ class ARTrainer(Seq2SeqTrainer):
             dataloader_params["drop_last"] = self.args.dataloader_drop_last
 
         return self.accelerator.prepare(DataLoader(eval_dataset, **dataloader_params))
-
-    def create_optimizer(self):
-        from transformers.pytorch_utils import ALL_LAYERNORM_LAYERS
-        from transformers.trainer_pt_utils import get_parameter_names
-
-        # overriden
-        opt_model = self.model_wrapped if is_sagemaker_mp_enabled() else self.model
-
-        if self.optimizer is None:
-            decay_parameters = get_parameter_names(opt_model, ALL_LAYERNORM_LAYERS)
-            decay_parameters = [name for name in decay_parameters if "bias" not in name]
-            optimizer_cls, optimizer_kwargs = Trainer.get_optimizer_cls_and_kwargs(
-                self.args
-            )
-
-            # only training warping parameters...
-            optimizer_grouped_parameters = [
-                {
-                    "params": [
-                        p
-                        for n, p in opt_model.named_parameters()
-                        if (
-                            n in decay_parameters
-                            and p.requires_grad
-                            and not ("cdf" in n or "linear_l" in n or "position_l" in n)
-                        )
-                    ],
-                    "weight_decay": self.args.weight_decay,
-                    "lr": optimizer_kwargs["lr"],
-                },
-                {
-                    "params": [
-                        p
-                        for n, p in opt_model.named_parameters()
-                        if (
-                            n not in decay_parameters
-                            and p.requires_grad
-                            and not ("cdf" in n or "linear_l" in n or "position_l" in n)
-                        )
-                    ],
-                    "weight_decay": 0.0,
-                    "lr": optimizer_kwargs["lr"],
-                },
-                {
-                    "params": [
-                        p
-                        for n, p in opt_model.named_parameters()
-                        if (
-                            ("cdf" in n or "linear_l" in n or "position_l" in n)
-                            and p.requires_grad
-                        )
-                    ],
-                    "weight_decay": 0.0,
-                    "lr": 1e-3,
-                },
-            ]
-
-            optimizer_kwargs.pop("lr")
-
-            self.optimizer = optimizer_cls(
-                optimizer_grouped_parameters, **optimizer_kwargs
-            )
-
-        return self.optimizer
