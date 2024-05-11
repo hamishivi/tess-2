a3d0b64c3ceb5942a8190b4b84c88ffef61923ac
Jake Tae <jaket@allennlp-cirrascale-50.reviz.ai2.in>
2024-05-11 01:24:45 +0000

    chore: sni ar gantry cmd

--- shell_scripts/run_pretrain.sh
+++ shell_scripts/run_pretrain.sh
@@ -192,72 +192,32 @@
 #         --from_scratch
 
 # jake: dolma mistral
-# gantry run -y -n fineweb_mistral_no_self_cond_no_mask_pad_lbl -t fineweb_mistral_no_self_cond_no_mask_pad_lbl --allow-dirty \
-#     --workspace ai2/tess2 \
-#     --nfs \
-#     --gpus 1 \
-#     --priority normal \
-#     --budget ai2/allennlp \
-#     --cluster ai2/allennlp-cirrascale \
-#     --env 'HF_HOME=/net/nfs.cirrascale/allennlp/jaket/.hf' \
-#     --env 'PROTOCOL_BUFFERS_PYTHON_IMPLEMENTATION=python' \
-#     --venv 'base' \
-#     --pip requirements.txt \
-#     -- python -m sdlm.run_mlm \
-#         --model_name_or_path mistralai/Mistral-7B-v0.1 \
-#         --per_device_train_batch_size 16  \
-#         --per_device_eval_batch_size 16 \
-#         --do_train \
-#         --do_eval \
-#         --output_dir /results \
-#         --evaluation_strategy steps \
-#         --eval_steps 2048 \
-#         --report_to tensorboard \
-#         --max_seq_length 512  \
-#         --max_eval_samples 512 \
-#         --simplex_value 5 \
-#         --num_diffusion_steps 5000  \
-#         --num_inference_diffusion_steps 10 100 200 \
-#         --lr_scheduler_type cosine \
-#         --learning_rate 1e-5 \
-#         --pad_to_max_length \
-#         --beta_schedule squaredcos_improved_ddpm \
-#         --weight_decay 0.01 \
-#         --top_p 0.99 \
-#         --max_steps 100000 \
-#         --gradient_accumulation_steps 4 \
-#         --warmup_ratio 0.05 \
-#         --logging_steps 50 \
-#         --save_steps 1000 \
-#         --save_total_limit 3 \
-#         --conditional_generation ul2 \
-#         --dataset_name HuggingFaceFW/fineweb --dataset_config_name CC-MAIN-2024-10 --streaming \
-#         --beaker \
-#         --bf16 \
-#         --optim adamw_torch_fused \
-#         --gradient_checkpointing \
-#         --use_flash_attention2 \
-#         --is_causal false \
-#         --line_by_line true \
-#         --eval_long_only true \
-#         --mask_padding_in_loss true
-
-
-python -m sdlm.run_mlm \
+gantry run -y -n fineweb_mistral -t fineweb_mistral --allow-dirty \
+    --workspace ai2/tess2 \
+    --nfs \
+    --gpus 1 \
+    --priority normal \
+    --budget ai2/allennlp \
+    --cluster ai2/allennlp-cirrascale \
+    --env 'HF_HOME=/net/nfs.cirrascale/allennlp/jaket/.hf' \
+    --env 'PROTOCOL_BUFFERS_PYTHON_IMPLEMENTATION=python' \
+    --venv 'base' \
+    --pip requirements.txt \
+    -- python -m sdlm.run_mlm \
         --model_name_or_path mistralai/Mistral-7B-v0.1 \
         --per_device_train_batch_size 16  \
         --per_device_eval_batch_size 16 \
         --do_train \
         --do_eval \
-        --output_dir outputs/mistral/test \
+        --output_dir /results \
         --evaluation_strategy steps \
-        --eval_steps 1 \
+        --eval_steps 512 \
         --report_to tensorboard \
         --max_seq_length 512  \
-        --max_eval_samples 16 \
+        --max_eval_samples 512 \
         --simplex_value 5 \
         --num_diffusion_steps 5000  \
-        --num_inference_diffusion_steps 100 500 \
+        --num_inference_diffusion_steps 10 100 200 \
         --lr_scheduler_type cosine \
         --learning_rate 1e-5 \
         --pad_to_max_length \
@@ -265,7 +225,7 @@ python -m sdlm.run_mlm \
         --weight_decay 0.01 \
         --top_p 0.99 \
         --max_steps 100000 \
-        --gradient_accumulation_steps 1 \
+        --gradient_accumulation_steps 4 \
         --warmup_ratio 0.05 \
         --logging_steps 50 \
         --save_steps 1000 \
@@ -279,4 +239,46 @@ python -m sdlm.run_mlm \
         --use_flash_attention2 \
         --is_causal false \
         --line_by_line true \
-        --mask_padding_in_loss false
+        --eval_long_only true \
+        --mask_padding_in_loss false \
+        --time_embed_lr 1e-4 \
+        --freeze_model true
+
+
+# python -m sdlm.run_mlm \
+#         --model_name_or_path mistralai/Mistral-7B-v0.1 \
+#         --per_device_train_batch_size 16  \
+#         --per_device_eval_batch_size 16 \
+#         --do_train \
+#         --do_eval \
+#         --output_dir outputs/mistral/test \
+#         --evaluation_strategy steps \
+#         --eval_steps 1 \
+#         --report_to tensorboard \
+#         --max_seq_length 512  \
+#         --max_eval_samples 16 \
+#         --simplex_value 5 \
+#         --num_diffusion_steps 5000  \
+#         --num_inference_diffusion_steps 100 500 \
+#         --lr_scheduler_type cosine \
+#         --learning_rate 1e-5 \
+#         --pad_to_max_length \
+#         --beta_schedule squaredcos_improved_ddpm \
+#         --weight_decay 0.01 \
+#         --top_p 0.99 \
+#         --max_steps 100000 \
+#         --gradient_accumulation_steps 1 \
+#         --warmup_ratio 0.05 \
+#         --logging_steps 50 \
+#         --save_steps 1000 \
+#         --save_total_limit 3 \
+#         --conditional_generation ul2 \
+#         --dataset_name HuggingFaceFW/fineweb --dataset_config_name CC-MAIN-2024-10 --streaming \
+#         --beaker \
+#         --bf16 \
+#         --optim adamw_torch_fused \
+#         --gradient_checkpointing \
+#         --use_flash_attention2 \
+#         --is_causal false \
+#         --line_by_line true \
+#         --mask_padding_in_loss false
