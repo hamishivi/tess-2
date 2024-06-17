3ae52831dbbbbbcb662b84b701428e61440d3b42
Jake Tae <jaesungtae@gmail.com>
2024-06-17 03:14:05 +0000

    chore: tulu cmd for jupiter

--- shell_scripts/run_tulu.sh
+++ shell_scripts/run_tulu.sh
@@ -4,15 +4,15 @@
 CMD="
 python -m sdlm.run_tulu \
     --dataset_name allenai/tulu-v2-sft-mixture \
-    --per_device_train_batch_size 4 \
-    --per_device_eval_batch_size 4 \
+    --per_device_train_batch_size 8 \
+    --per_device_eval_batch_size 8 \
     --evaluation_strategy steps \
     --do_train \
     --do_eval \
     --num_train_epochs 2 \
     --report_to tensorboard \
     --overwrite_output_dir \
-    --max_seq_length 1024 \
+    --max_seq_length 512 \
     --simplex_value 5 \
     --num_diffusion_steps 5000 \
     --lr_scheduler_type cosine \
@@ -37,15 +37,53 @@ python -m sdlm.run_tulu \
     --skip_special_tokens false \
 "
 
+# for ai2/allennlp-cirrascale cluster
+# if [ ! -z "${BEAKER}" ]; then
+#     gantry run -y -n tulu_mistral_dolma_adapt -t tulu_mistral_dolma_adapt --allow-dirty \
+#         --workspace ai2/tess2 \
+#         --nfs \
+#         --gpus 1 \
+#         --priority normal \
+#         --budget ai2/allennlp \
+#         --cluster ai2/allennlp-cirrascale \
+#         --env 'HF_HOME=/net/nfs.cirrascale/allennlp/jaket/.hf' \
+#         --env 'PROTOCOL_BUFFERS_PYTHON_IMPLEMENTATION=python' \
+#         --env 'IS_ALPACA_EVAL_2=False' \
+#         --env-secret OPENAI_API_KEY=OPENAI_API_KEY \
+#         --beaker-image 'ai2/pytorch2.0.0-cuda11.8-python3.10' \
+#         --venv 'base' \
+#         --pip requirements.txt \
+#         -- ${CMD} \
+#         --model_name_or_path /net/nfs.cirrascale/allennlp/jaket/simplex-diffusion/outputs/dolma_mistral/checkpoint-22000 \
+#         --eval_steps 1000 \
+#         --save_steps 500 \
+#         --max_eval_samples 1000 \
+#         --gradient_accumulation_steps 16 \
+#         --num_inference_diffusion_steps 50 100 200 \
+#         --beaker \
+#         --output_dir /results
+# else
+#     ${CMD} \
+#         --model_name_or_path mistralai/Mistral-7B-v0.1 \
+#         --eval_steps 3 \
+#         --save_steps 5 \
+#         --max_eval_samples 16 \
+#         --gradient_accumulation_steps 1 \
+#         --num_inference_diffusion_steps 10 \
+#         --output_dir outputs/test
+# fi
+
+# for ai2/jupiter-cirrascale-2 cluster
 if [ ! -z "${BEAKER}" ]; then
-    gantry run -y -n tulu_mistral_dolma_adapt -t tulu_mistral_dolma_adapt --allow-dirty \
+    gantry run -y -n tulu_mistral_dolma_adapt_100k -t tulu_mistral_dolma_adapt_100k --allow-dirty \
         --workspace ai2/tess2 \
-        --nfs \
-        --gpus 1 \
+        --gpus 8 \
         --priority normal \
         --budget ai2/allennlp \
-        --cluster ai2/allennlp-cirrascale \
-        --env 'HF_HOME=/net/nfs.cirrascale/allennlp/jaket/.hf' \
+        --preemptible \
+        --no-nfs \
+        --cluster ai2/jupiter-cirrascale-2 \
+        --env 'HF_HOME=/net/weka/reviz/jaket/.hf' \
         --env 'PROTOCOL_BUFFERS_PYTHON_IMPLEMENTATION=python' \
         --env 'IS_ALPACA_EVAL_2=False' \
         --env-secret OPENAI_API_KEY=OPENAI_API_KEY \
@@ -53,12 +91,12 @@ if [ ! -z "${BEAKER}" ]; then
         --venv 'base' \
         --pip requirements.txt \
         -- ${CMD} \
-        --model_name_or_path /net/nfs.cirrascale/allennlp/jaket/simplex-diffusion/outputs/dolma_mistral/checkpoint-22000 \
+        --model_name_or_path outputs/dolma_mistral_512/100k/checkpoint-100000 \
         --eval_steps 1000 \
-        --save_steps 500 \
+        --save_steps 1000 \
         --max_eval_samples 1000 \
-        --gradient_accumulation_steps 16 \
-        --num_inference_diffusion_steps 50 100 200 \
+        --gradient_accumulation_steps 1 \
+        --num_inference_diffusion_steps 100 \
         --beaker \
         --output_dir /results
 else
