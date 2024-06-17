9f90469a3b5c1b9fd125bc83c05a8c8eb7372c37
Hamish Ivison <hamishivi@gmail.com>
2024-06-17 09:58:39 -0700

    first pass on rm training

---
+++ shell_scripts/run_train_rm.sh
@@ -0,0 +1,46 @@
+CMD="
+python -m sdlm.train_reward_model \
+    --model_name_or_path mistralai/Mistral-7B-v0.1 \
+    --num_train_epochs 1 \
+    --per_device_train_batch_size 8 \
+    --remove_unused_columns=False \
+    --gradient_checkpointing=True \
+    --warmup_ratio 0.03 \
+    --learning_rate=2e-5 \
+    --report_to="tensorboard" \
+    --logging_steps=50 \
+    --save_total_limit 1 \
+    --eval_strategy="no" \
+    --max_length=512 \
+    --attn_implementation=flash_attention_2 \
+    --gradient_checkpointing \
+    --include_padding=False \
+    --use_tulu_chat_template=True \
+"
+
+if [ ! -z "${BEAKER}" ]; then
+    gantry run -y -n mistral_rm_train -t mistral_rm_train --allow-dirty \
+        --workspace ai2/tess2 \
+        --nfs \
+        --gpus 1 \
+        --priority normal \
+        --budget ai2/allennlp \
+        --cluster ai2/allennlp-cirrascale \
+        --env 'HF_HOME=/net/nfs.cirrascale/allennlp/jaket/.hf' \
+        --env 'PROTOCOL_BUFFERS_PYTHON_IMPLEMENTATION=python' \
+        --beaker-image 'ai2/pytorch2.0.0-cuda11.8-python3.10' \
+        --venv 'base' \
+        --pip requirements.txt \
+        -- ${CMD} \
+        --eval_steps 200 \
+        --save_steps 400 \
+        --gradient_accumulation_steps 64 \
+        --beaker \
+        --output_dir /results
+else
+    ${CMD} \
+        --eval_steps 1 \
+        --save_steps 5 \
+        --gradient_accumulation_steps 1 \
+        --output_dir outputs/test
+fi
