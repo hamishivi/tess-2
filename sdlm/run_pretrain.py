7841ec4f3348e0ffb2180120890cff1e19a86943
Jake Tae <jaesungtae@gmail.com>
2024-07-03 00:42:45 +0000

    fix: train from last ckpt

--- sdlm/run_pretrain.py
+++ sdlm/run_pretrain.py
@@ -270,12 +270,7 @@ def main():
 
     # Training
     if training_args.do_train:
-        checkpoint = None
-        if training_args.resume_from_checkpoint is not None:
-            checkpoint = training_args.resume_from_checkpoint
-        elif last_checkpoint is not None:
-            checkpoint = last_checkpoint
-        train_result = trainer.train(resume_from_checkpoint=checkpoint)
+        train_result = trainer.train(resume_from_checkpoint=last_checkpoint)
         trainer.save_model()  # Saves the tokenizer too for easy upload
         metrics = train_result.metrics
 
