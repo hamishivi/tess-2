40ed337acadd728447ca2bb730f6a3d77786a3f5
Hamish Ivison <hamishivi@gmail.com>
2025-02-05 10:56:55 -0800

    add helper funcs

--- sdlm/data/instruction_evals/instruction_evals.py
+++ sdlm/data/instruction_evals/instruction_evals.py
@@ -816,6 +816,38 @@ def calculate_scores(outputs):
     }
 
 
+choices = ["A", "B", "C", "D"]
+
+
+def format_subject(subject):
+    l = subject.split("_")
+    s = ""
+    for entry in l:
+        s += " " + entry
+    return s
+
+
+def format_example(df, idx, include_answer=True):
+    prompt = df.iloc[idx, 0]
+    k = df.shape[1] - 2
+    for j in range(k):
+        prompt += "\n{}. {}".format(choices[j], df.iloc[idx, j + 1])
+    prompt += "\nAnswer:"
+    if include_answer:
+        prompt += " {}\n\n".format(df.iloc[idx, k + 1])
+    return prompt
+
+
+def gen_prompt(train_df, subject, k=-1):
+    prompt = "The following are multiple choice questions (with answers) about {}.\n\n".format(
+        format_subject(subject)
+    )
+    if k == -1:
+        k = train_df.shape[0]
+    for i in range(k):
+        prompt += format_example(train_df, i)
+    return prompt
+
 class MMLUEval():
     def compute_metrics(results, skip_special_tokens=True):
         # Extract prefixes and predictions
